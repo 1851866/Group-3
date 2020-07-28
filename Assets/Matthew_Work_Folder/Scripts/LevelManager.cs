88bfdc9aa0950131ae3c9097469293845754435c
190BlackDevil091 <53477873+190BlackDevil091@users.noreply.github.com>
2020-07-28 02:12:24 +0200

    Basic_Turret_Movement+Level_Manager

---
+++ Assets/Matthew_Work_Folder/Scripts/LevelManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public string gameOverSceneName;
+    public string menuSceneName;
+    public string gameSceneName;
+    public string creditsSceneName;
+    public string helpSceneName;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(menuSceneName);
+        }
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(menuSceneName); 
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(creditsSceneName);
+    }
+
+    public void Help()
+    {
+        SceneManager.LoadScene(helpSceneName);
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(gameOverSceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Matthew_Work_Folder/Scripts/TurretControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretControl : MonoBehaviour
+{
+    public Transform turretBody;
+    public Transform turretGun;
+
+    public float turnSpeed;
+    public float maxLeftRotate;
+    public float maxRightRotate;
+    public float maxHeight;
+    public float minHeight;
+
+    private float sideAngle = 0;
+    private float verticalAngle = 0;
+
+    private void Update()
+    {
+        RotateTurret();
+    }
+
+    private void RotateTurret()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            sideAngle += turnSpeed;
+        }
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            sideAngle -= turnSpeed;
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            verticalAngle += turnSpeed;
+        }
+
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            verticalAngle -= turnSpeed;
+        }
+
+        verticalAngle = Mathf.Clamp(verticalAngle, minHeight, maxHeight);
+        sideAngle = Mathf.Clamp(sideAngle, maxLeftRotate, maxRightRotate);
+        turretBody.eulerAngles = new Vector3(verticalAngle, sideAngle, 0);
+    }
+
+}
