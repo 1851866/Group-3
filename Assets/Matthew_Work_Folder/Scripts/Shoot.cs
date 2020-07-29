16d75def40972e83e31ee4e5678697df63eb53c8
190BlackDevil091 <53477873+190BlackDevil091@users.noreply.github.com>
2020-07-29 11:11:46 +0200

    FireScript

---
+++ Assets/Matthew_Work_Folder/Scripts/Shoot.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public GameObject turretHead;
+
+    public Transform firePoint1;
+    public Transform firePoint2;
+
+    public float bulletSpeed;
+    public float fireRate;
+
+    private bool firing = false;
+    private Coroutine fireCoroutine;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (firing == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                firing = true;
+                fireCoroutine = StartCoroutine("continuousFire");
+            }
+        }
+        if (firing == true)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                firing = false;
+                StopCoroutine(fireCoroutine);
+            }
+        }
+    }
+
+    IEnumerator continuousFire()
+    {
+        while (true)
+        {
+            FireGun();
+            yield return new WaitForSeconds(fireRate);
+        }
+    }
+
+    private void FireGun()
+    {
+        GameObject bullet1 = Instantiate(bulletPrefab, firePoint1.position, Quaternion.identity) as GameObject;
+        GameObject bullet2 = Instantiate(bulletPrefab, firePoint2.position, Quaternion.identity) as GameObject;
+        bullet1.GetComponent<Rigidbody>().AddForce(bullet1.transform.forward * bulletSpeed);
+        bullet2.GetComponent<Rigidbody>().AddForce(bullet2.transform.forward * bulletSpeed);
+    }
+}
