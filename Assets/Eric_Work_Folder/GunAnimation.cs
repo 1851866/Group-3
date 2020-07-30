61e1421be35abb643b8f4a7699ba158586cac93b
1851866 <1851866@users.noreply.github.com>
2020-07-30 11:20:13 +0200

    Added VFX

--- Assets/Eric_Work_Folder/GunAnimation.cs
+++ Assets/Eric_Work_Folder/GunAnimation.cs
@@ -7,6 +7,8 @@ public class GunAnimation : MonoBehaviour
     // Start is called before the first frame update
 
     Animator Gun_A;
+    public ParticleSystem Flash1;
+    public ParticleSystem Flash2;
     void Start()
     {
         Gun_A = gameObject.GetComponent<Animator>();
@@ -17,9 +19,10 @@ public class GunAnimation : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            
+            Flash1.Play();
+            Flash2.Play();
             Gun_A.SetInteger("Shoot", 1);
-            Debug.Log("Shooting");
+            //Debug.Log("Shooting");
         }
 
         if (Input.GetMouseButtonUp(0))
